1006e9a6925f7b82c666ca8970d5914abfec54e0
wkd3475 <wkd3475@gmail.com>
2019-01-31 15:12:35 +0900

    Making DB

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from polls.models import Post
 
 # Register your models here.
+admin.site.register(Post)
--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=30)
+    context = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.title
