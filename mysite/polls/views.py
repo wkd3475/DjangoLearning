4f94d31da4150825f761107577ee8e117300049a
wkd3475 <wkd3475@gmail.com>
2019-02-03 16:56:32 +0900

    modify the name of variablein views.py. 'published_date' => 'created_date'

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -4,5 +4,5 @@ from .models import Post
 
 # Create your views here.
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts = Post.objects.filter(created_date__lte=timezone.now()).order_by('created_date')
     return render(request, 'polls/post_list.html', {'posts': posts})
